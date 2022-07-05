2b17d105fbbc6b1fd5e55f30ef6c16b8755bbb67
Red <crabmarket56@gmail.com>
2022-07-05 02:34:20 -0400

    Add operator buttons and draft functionality

--- external-js.js
+++ external-js.js
@@ -13,3 +13,35 @@ const multiply = (a,b) => {
 const divide = (a,b) => {
     return a / b;
     };
+
+
+    let operators = document.querySelectorAll("[data-operator]");
+
+    const OPERATORS = [
+    "+", "-", "*", '/'
+    ]
+
+
+const operate = (a, operator, b) => {
+    operators.forEach(operator => {operator.addEventListener('click', e => {
+    let select = operator.dataset.selection
+    const operator =  OPERATORS.find(operator => operator === select)
+    switch(operator) {
+        case '+':
+            return add(a,b)
+            break;
+            
+        case '-':
+            return subtract(a,b)
+            break;
+        
+        case '*':
+            return multiply(a,b)
+            break;
+
+        case '/':
+            return divide(a,b)
+            break;
+    }
+});
+})}
