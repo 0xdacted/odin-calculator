c2c1123053700f9f3fd8f30af996361411a83559
Red <crabmarket56@gmail.com>
2022-07-05 02:05:57 -0400

    Parse-ints would not return decimals, removed

--- external-js.js
+++ external-js.js
@@ -1,15 +1,15 @@
 const add = (a,b) => { 
-    return parseInt(a + b);
+    return a + b;
   };
 
-  const subtract = (a,b) => {
-	return parseInt(a - b);
+const subtract = (a,b) => {
+	return a - b;
 };
 
 const multiply = (a,b) => {
-    return parseInt(a * b);
+    return a * b;
     };
 
 const divide = (a,b) => {
-    return parseInt(a / b);
+    return a / b;
     };
